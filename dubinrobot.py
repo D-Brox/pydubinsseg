5c21ebc0e8af68bce711a06ce67b4c7bcebc8252
David Brochero <davidfbg00@gmail.com>
2024-03-31 19:37:36 +0000

    Remove lidar

--- dubinrobot.py
+++ dubinrobot.py
@@ -29,22 +29,3 @@ class DubinRobot():
         w = Kp*theta_error
         v = self.__constant_linear_velocity * delta
         return (v,w)
-
-    # def update_measurement(self,measurement):
-    #     self.range_sensor = measurement
-
-    # def get_obstacles_position(self):
-    #     obstacles_position = []
-    #     for dist,angle in self.range_sensor.get_ranges_with_angle():
-    #         xo = self.x + dist*cos(self.theta + angle)
-    #         yo = self.y + dist*sin(self.theta + angle)
-    #         obstacles_position.append( (xo,yo) )
-    #         # msg = f"Robot {self.get_number()},{self.group} Measurement: {[(dist,angle),(xo,yo),(self.x, self.y, self.theta)]}"
-    #         # rospy.loginfo(msg)
-    #     return obstacles_position
-
-
-# Unit test
-if __name__ == "__main__":
-    # N/A
-	pass
